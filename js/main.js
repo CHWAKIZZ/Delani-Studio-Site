e39207cf463f7a7bc13f2e476fbeec57bc49e618
DMG <dmwaigithinji@gmail.com>
2019-05-06 01:25:30 +0300

    Add feature to toggle between illustration and details in 'what we do' section

--- js/main.js
+++ js/main.js
@@ -1,13 +1,37 @@
 $(document).ready(function() {
-$('#portfolio span').addClass( "hide" );
-$('#portfolio .doings').hover(function() {
-    /*fade in code*/
-    $(this).find('span').removeClass('hide');
-    $(this).find('.doings').addClass('imageEffect');
-}, function() {
-    /*fade out code*/
-    $(this).find('span').addClass('hide');
-    $(this).find('.doings').removeClass('imageEffect');
-});
+    $('#portfolio span').addClass( "hide" );
+    $('#portfolio .doings').hover(function() {
+        /*fade in code*/
+        $(this).find('span').removeClass('hide');
+        $(this).find('.doings').addClass('imageEffect');
+    }, function() {
+        /*fade out code*/
+        $(this).find('span').addClass('hide');
+        $(this).find('.doings').removeClass('imageEffect');
+    });
 
+    $(".illustration1").click(function(){
+        $(".illustration1").toggle(500);
+        $(".details1").toggle(500);
+    });
+    $(".details1").click(function(){
+        $(".details1").toggle(500);
+        $(".illustration1").toggle(500);
+    });
+    $(".illustration2").click(function(){
+        $(".illustration2").toggle(500);
+        $(".details2").toggle(500);
+    });
+    $(".details2").click(function(){
+        $(".details2").toggle(500);
+        $(".illustration2").toggle(500);
+    });
+    $(".illustration3").click(function(){
+        $(".illustration3").toggle(500);
+        $(".details3").toggle(500);
+    });
+    $(".details3").click(function(){
+        $(".details3").toggle(500);
+        $(".illustration3").toggle(500);
+    });
 });
