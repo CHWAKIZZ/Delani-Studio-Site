7fc5842be185214b66ca652ddc49aaa8224fed18
DMG <dmwaigithinji@gmail.com>
2019-05-06 00:24:45 +0300

    Add hover effect to reveal work label on portfolio

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+$('#portfolio span').addClass( "hide" );
+$('#portfolio .doings').hover(function() {
+    /*fade in code*/
+    $(this).find('span').removeClass('hide');
+    $(this).find('.doings').addClass('imageEffect');
+}, function() {
+    /*fade out code*/
+    $(this).find('span').addClass('hide');
+    $(this).find('.doings').removeClass('imageEffect');
+});
+
+});
