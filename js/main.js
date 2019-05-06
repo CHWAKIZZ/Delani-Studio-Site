8b22491bc34f849ed734325369b6f3da6f3a2488
DMG <dmwaigithinji@gmail.com>
2019-05-06 05:19:30 +0300

    Make indentation corrections

--- js/main.js
+++ js/main.js
@@ -1,95 +1,71 @@
 $(document).ready(function() {
-    // Transition effect for navbar 
-    $(window).scroll(function() {
-        // checks if window is scrolled more than 500px, adds/removes solid class
-        if($(this).scrollTop() > 600) { 
-            $('.navbar').addClass('solid');
-        } else {
-            $('.navbar').removeClass('solid');
-        }
-    });
-
-    //Portfolio label on hover effect
-    $('#portfolio span').addClass( "hide" );
-    $('#portfolio .doings').hover(function() {
-        /*fade in code*/
-        $(this).find('span').removeClass('hide');
-        $(this).find('.doings').addClass('imageEffect');
-    }, function() {
-        /*fade out code*/
-        $(this).find('span').addClass('hide');
-        $(this).find('.doings').removeClass('imageEffect');
-    });
-
-    //What we do description toggle
-    $(".illustration1").click(function(){
-        $(".illustration1").toggle(500);
-        $(".details1").toggle(500);
-    });
-    $(".details1").click(function(){
-        $(".details1").toggle(500);
-        $(".illustration1").toggle(500);
-    });
-    $(".illustration2").click(function(){
-        $(".illustration2").toggle(500);
-        $(".details2").toggle(500);
-    });
-    $(".details2").click(function(){
-        $(".details2").toggle(500);
-        $(".illustration2").toggle(500);
-    });
-    $(".illustration3").click(function(){
-        $(".illustration3").toggle(500);
-        $(".details3").toggle(500);
-    });
-    $(".details3").click(function(){
-        $(".details3").toggle(500);
-        $(".illustration3").toggle(500);
-    });
-
-    $('#submit').submit(function(event){
-        var name = $('#name').val();
-        var email = $('#email').val();
-        var message = $('#message').val();
-        if(name == "" || email == ""|| message == "" ) {
-            $("#message").text("Please fill in all the fields with correct data.");
-            $('#message').removeClass("success");
-            $('#message').addClass("failed");
-        } 
-        else {
-            $("#message").text("Thank you "+ name + ".<br>Your message has been successfully submitted.");
-            $('#message').removeClass("failed");
-            $('#message').addClass("success");
-            }
-        event.preventDefault();
-        });
-
-
-
-var scrollLink = $('.scroll');
-// Smooth scrolling
-scrollLink.click(function(e) {
-    e.preventDefault();
-    $('body,html').animate({
-    scrollTop: $(this.hash).offset().top
-    }, 1000 );
-});
-  // Active link switching
-    $(window).scroll(function() {
-    var scrollbarLocation = $(this).scrollTop();
-    scrollLink.each(function() {
-    var sectionOffset = $(this.hash).offset().top - 20;
-    if ( sectionOffset <= scrollbarLocation ) {
-        $(this).parent().addClass('active');
-        $(this).parent().siblings().removeClass('active');
+  // Transition effect for navbar
+  $(window).scroll(function() {
+    // checks if window is scrolled more than 500px, adds/removes solid class
+    if ($(this).scrollTop() > 600) {
+      $('.navbar').addClass('solid');
+    } else {
+      $('.navbar').removeClass('solid');
     }
-    })
-})
-
-    $(function() {
-        $('a[href*=#]').on('click', function(e) {
-            e.preventDefault();
-            $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 300, 'linear');
-        });
+  });
+  //Portfolio label on hover effect
+  $('#portfolio span').addClass("hide");
+  $('#portfolio .doings').hover(function() {
+    /*fade in code*/
+    $(this).find('span').removeClass('hide');
+    $(this).find('.doings').addClass('imageEffect');
+  }, function() {
+    /*fade out code*/
+    $(this).find('span').addClass('hide');
+    $(this).find('.doings').removeClass('imageEffect');
+  });
+  //What we do description toggle
+  $(".illustration1").click(function() {
+    $(".illustration1").toggle(500);
+    $(".details1").toggle(500);
+  });
+  $(".details1").click(function() {
+    $(".details1").toggle(500);
+    $(".illustration1").toggle(500);
+  });
+  $(".illustration2").click(function() {
+    $(".illustration2").toggle(500);
+    $(".details2").toggle(500);
+  });
+  $(".details2").click(function() {
+    $(".details2").toggle(500);
+    $(".illustration2").toggle(500);
+  });
+  $(".illustration3").click(function() {
+    $(".illustration3").toggle(500);
+    $(".details3").toggle(500);
+  });
+  $(".details3").click(function() {
+    $(".details3").toggle(500);
+    $(".illustration3").toggle(500);
+  });
+  //Submission Form
+  $('#submit').submit(function(event) {
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var message = $('#message').val();
+    if (name == "" || email == "" || message == "") {
+      $("#message").text("Please fill in all the fields with correct data.");
+      $('#message').removeClass("success");
+      $('#message').addClass("failed");
+    } else {
+      $("#message").text("Thank you " + name + ".<br>Your message has been successfully submitted.");
+      $('#message').removeClass("failed");
+      $('#message').addClass("success");
+    }
+    event.preventDefault();
+  });
+  $(function() {
+    $('a[href*=#]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+      }, 300, 'linear');
     });
+  });
 });
